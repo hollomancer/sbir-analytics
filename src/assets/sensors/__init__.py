bd0b72dc8ca8f3bf5afd85a6f6da07a03c1ebfb9
Conrad Hollomon <hollomancer@protonmail.ch>
2025-12-01 14:34:33 -0500

    Remove S3 sensor (not needed without Dagster Cloud)
    - GitHub Actions data-refresh workflow triggers Dagster directly
    - S3 sensor was designed for Dagster Cloud which is no longer used

--- src/assets/sensors/__init__.py
+++ src/assets/sensors/__init__.py
@@ -1,7 +1,6 @@
 """Dagster sensors package for SBIR ETL pipeline."""
 
-from .s3_data_sensor import s3_sbir_data_sensor
 from .usaspending_refresh_sensor import usaspending_refresh_sensor
 
 
-__all__ = ["usaspending_refresh_sensor", "s3_sbir_data_sensor"]
+__all__ = ["usaspending_refresh_sensor"]
