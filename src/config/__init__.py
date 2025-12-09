77995dcf14e686113856df435156cc8e6056deef
Conrad Hollomon <hollomancer@protonmail.ch>
2025-12-09 08:28:27 -0500

    Fix load_config() for notebooks
    Add convenience function that works without arguments by
    calling get_config() internally. Fixes notebook execution.

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,5 +1,14 @@
 """Configuration loading utilities."""
 
-from src.config.loader import load_config_from_files as load_config
+from src.config.loader import get_config, load_config_from_files
 
-__all__ = ["load_config"]
+
+def load_config():
+    """Convenience function to load config with default paths.
+
+    For use in notebooks and scripts where you don't need to specify paths.
+    """
+    return get_config()
+
+
+__all__ = ["load_config", "load_config_from_files", "get_config"]
