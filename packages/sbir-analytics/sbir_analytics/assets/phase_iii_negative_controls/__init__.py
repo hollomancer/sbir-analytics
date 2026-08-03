289a90e671beb3c8a21d73fe88676f0235d06f2a
Conrad Hollomon <hollomancer@protonmail.ch>
2026-08-03 07:39:03 -0400

    Audit unresolved award quarantine keys

--- packages/sbir-analytics/sbir_analytics/assets/phase_iii_negative_controls/__init__.py
+++ packages/sbir-analytics/sbir_analytics/assets/phase_iii_negative_controls/__init__.py
@@ -8,6 +8,13 @@ from .identity import (
     resolve_award_identities,
 )
 from .nih_reporter import NIHReporterExtractor
+from .quarantine import (
+    QuarantineKeyCoverage,
+    build_unresolved_quarantine_key_audit,
+    quarantine_key_gate,
+    require_complete_unresolved_quarantine_keys,
+    summarize_quarantine_key_coverage,
+)
 from .source_keys import (
     build_nih_official_keys,
     build_nih_sbir_attempts,
@@ -20,10 +27,15 @@ __all__ = [
     "RecoveryStatus",
     "FebruaryAwardSearchExtractor",
     "NIHReporterExtractor",
+    "QuarantineKeyCoverage",
     "build_nih_official_keys",
     "build_nih_sbir_attempts",
     "build_usaspending_official_keys",
     "build_usaspending_sbir_attempts",
+    "build_unresolved_quarantine_key_audit",
+    "quarantine_key_gate",
     "reconcile_award_identity_attempts",
+    "require_complete_unresolved_quarantine_keys",
     "resolve_award_identities",
+    "summarize_quarantine_key_coverage",
 ]
